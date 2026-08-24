3c3f4cfa3aa571ae7223db2ba3fb1822b2062e60
AbdullaK123 <abdullamaseeh123@gmail.com>
2026-08-24 11:05:15 +0400

    added missing exports

--- wire_rpc/__init__.py
+++ wire_rpc/__init__.py
@@ -3,11 +3,13 @@ from .multicast_app import MulticastApp
 from .client import Client
 from .request import WireRequest
 from .response import WireResponse, WireErrorResponse, WireSuccessResponse
+from .router import Router
 
 __all__ = [
     "App",
     "MulticastApp",
     "Client",
+    "Router",
     "WireRequest",
     "WireResponse",
     "WireErrorResponse",
