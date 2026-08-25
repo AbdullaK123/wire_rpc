e0d504ba400765467f6d4e6a1755ebd5b29b48ec
Abdalla Kayyani <121222897+AbdullaK123@users.noreply.github.com>
2026-08-25 21:59:33 +0400

    export tcp keepalive config

--- wire_rpc/transports/tcp/__init__.py
+++ wire_rpc/transports/tcp/__init__.py
@@ -1,7 +1,13 @@
-from .tcp import *
+from .keep_alive import TcpKeepaliveConfig
+from .tcp import (
+    TcpClientTransport,
+    TcpMulticastServerTransport,
+    TcpServerTransport,
+)
 
 __all__ = [
+    "TcpClientTransport",
     "TcpMulticastServerTransport",
     "TcpServerTransport",
-    "TcpClientTransport",
+    "TcpKeepaliveConfig",
 ]
